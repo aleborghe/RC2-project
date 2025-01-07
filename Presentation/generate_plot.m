9e71f2da253655c6e98884162e466f7a01ec64c9
thomas397 <64840739+thomas397@users.noreply.github.com>
2025-01-07 23:47:26 +0100

    change

--- Presentation/generate_plot.m
+++ Presentation/generate_plot.m
@@ -12,7 +12,7 @@ theta1 = out.uni_state1.signals.values(:, 3); % Orientation (theta) (second stru
 time1 = out.uni_state.time;
 time2 = out.uni_state1.time;
 
-ref_signals = reshape(permute(out.reference.signals.values, [3, 1, 2]), 1000, 3);
+ref_signals = reshape(permute(out.reference.signals.values, [3, 1, 2]), 1006, 3);
 
 x_ref = ref_signals(:, 1);
 y_ref = ref_signals(:, 2); % Reference y
@@ -30,7 +30,7 @@ grid on;
 grid minor;
 
 % Small box with zoomed part
-axes('position', [.65 .45 .25 .25]); % Position of the zoom box
+axes('position', [.25 .25 .25 .25]); % Position of the zoom box
 box on; % Put box around new pair of axes
 indexOfInterest = (time1 >= 4.5) & (time1 <= 5.5); % Range of time to zoom
 plot(time1(indexOfInterest), x_ref(indexOfInterest), 'b-');
@@ -53,7 +53,7 @@ grid on;
 grid minor;
 
 % Small box with zoomed part
-axes('position', [.65 .45 .25 .25]); % Position of the zoom box
+axes('position', [.50 .25 .25 .25]); % Position of the zoom box
 box on; % Put box around new pair of axes
 indexOfInterest = (time1 >= 4.8) & (time1 <= 5.2); % Range of time to zoom
 plot(time1(indexOfInterest), y_ref(indexOfInterest), 'b-');
@@ -76,14 +76,27 @@ grid on;
 grid minor;
 
 % Small box with zoomed part
-axes('position', [.65 .25 .25 .25]); % Position of the zoom box
+axes('position', [.60 .18 .25 .35]); % Position of the zoom box
 box on; % Put box around new pair of axes
 indexOfInterest = (time1 >= 4.8) & (time1 <= 5.2); % Range of time to zoom
 plot(time1(indexOfInterest), theta_ref(indexOfInterest), 'b-');
 hold on;
 plot(time1(indexOfInterest), theta(indexOfInterest), 'r-');
 axis tight;
-ylim([1.2 3.3]); % Adjust y-axis limits for the zoomed-in plot
+ylim([1.2 4.8]); % Adjust y-axis limits for the zoomed-in plot
+grid on; % Ensure grid is on in zoomed area
+grid minor; % Minor grid lines in zoomed area
+hold off;
+
+% Small box with zoomed part
+axes('position', [.15 .65 .25 .25]); % Position of the zoom box
+box on; % Put box around new pair of axes
+indexOfInterest = (time1 >= 0.8) & (time1 <= 2.5); % Range of time to zoom
+plot(time1(indexOfInterest), theta_ref(indexOfInterest), 'b-');
+hold on;
+plot(time1(indexOfInterest), theta(indexOfInterest), 'r-');
+axis tight;
+ylim([-1.5 1.5]); % Adjust y-axis limits for the zoomed-in plot
 grid on; % Ensure grid is on in zoomed area
 grid minor; % Minor grid lines in zoomed area
 hold off;
