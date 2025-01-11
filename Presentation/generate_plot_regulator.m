f8154cf76f502f24a1a888707c37be3d3ff719ff
aleborghe <aleborgherini@gmail.com>
2025-01-11 12:16:23 +0100

    Update

--- Presentation/generate_plot_regulator.m
+++ Presentation/generate_plot_regulator.m
@@ -1,7 +1,9 @@
+signals = reshape(permute(out.uni_state1.signals.values, [3, 1, 2]), 1251, 3);
+
 % First structure
-x = out.uni_state1.signals.values(:, 1); % Coordinate x (first structure)
-y = out.uni_state1.signals.values(:, 2); % Coordinate y (first structure)
-theta = out.uni_state1.signals.values(:, 3); % Orientation (theta) (first structure)
+x = signals(:, 1); % Coordinate x (first structure)
+y = signals(:, 2); % Coordinate y (first structure)
+theta = signals(:, 3); % Orientation (theta) (first structure)
 
 % % Second structure
 % x1 = out.uni_state2.signals.values(:, 1); % Coordinate x (second structure)
@@ -12,43 +14,43 @@ theta = out.uni_state1.signals.values(:, 3); % Orientation (theta) (first struct
 time1 = out.uni_state1.time;
 %time2 = out.uni_state2.time;
 
-ref_signals = reshape(permute(out.reference1.signals.values, [3, 1, 2]), 1251, 3);
+%ref_signals = reshape(permute(out.reference1.signals.values, [3, 1, 2]), 1251, 3);
 
-x_ref = ref_signals(:, 1);
-y_ref = ref_signals(:, 2); % Reference y
-theta_ref = ref_signals(:, 3);
-
+% x_ref = ref_signals(:, 1);
+% y_ref = ref_signals(:, 2); % Reference y
+% theta_ref = ref_signals(:, 3);
+index = (time1 <= 12.5);
 % Create the plot for X
 figure;
-plot(time1, x_ref, 'b-');
+%plot(time1(index), x_ref(index), 'b-');
 hold on;
-plot(time1, x, 'r-');
+plot(time1(index), x(index), 'r-');
 xlabel('Time [s]');
-ylabel('X position [m]');
-legend("Reference x(t)", "x(t)");         % Add legend for clarity
+ylabel('Roh');
+legend("Roh");         % Add legend for clarity
 grid on;
 grid minor;
 
-% Small box with zoomed part
-axes('position', [.65 .45 .25 .25]); % Position of the zoom box
-box on; % Put box around new pair of axes
-indexOfInterest = (time1 >= 9.9) & (time1 <= 10.6); % Range of time to zoom
-plot(time1(indexOfInterest), x_ref(indexOfInterest), 'b-');
-hold on;
-plot(time1(indexOfInterest), x(indexOfInterest), 'r-');
-axis tight;
-grid on; % Ensure grid is on in zoomed area
-grid minor; % Minor grid lines in zoomed area
-hold off;
+% % Small box with zoomed part
+% axes('position', [.65 .45 .25 .25]); % Position of the zoom box
+% box on; % Put box around new pair of axes
+% indexOfInterest = (time1 >= 9.9) & (time1 <= 10.6); % Range of time to zoom
+% plot(time1(indexOfInterest), x_ref(indexOfInterest), 'b-');
+% hold on;
+% plot(time1(indexOfInterest), x(indexOfInterest), 'r-');
+% axis tight;
+% grid on; % Ensure grid is on in zoomed area
+% grid minor; % Minor grid lines in zoomed area
+% hold off;
 
 % Create the plot for Y
 figure;
-plot(time1, y_ref, 'b-');
+plot(time1(index), theta(index), 'b-');
 hold on;
-plot(time1, y, 'r-');
+plot(time1(index), y(index), 'r-');
 xlabel('Time [s]');
-ylabel('Y position [m]');
-legend("Reference y(t)", "y(t)");         % Add legend for clarity
+ylabel('Gamma, delta [rad]');
+legend("Gamma(t)", "Delta(t)");         % Add legend for clarity
 grid on;
 grid minor;
 
@@ -64,16 +66,16 @@ grid minor;
 % grid minor; % Minor grid lines in zoomed area
 % hold off;
 
-% Create the plot for Theta
-figure;
-plot(time1, theta_ref, 'b-');
-hold on;
-plot(time1, theta, 'r-');
-xlabel('Time [s]');
-ylabel('Theta position [rad]');
-legend("Reference theta(t)", "theta(t)");         % Add legend for clarity
-grid on;
-grid minor;
+% % Create the plot for Theta
+% figure;
+% plot(time1(index), theta_ref(index), 'b-');
+% hold on;
+% plot(time1(index), theta(index), 'r-');
+% xlabel('Time [s]');
+% ylabel('Theta position [rad]');
+% legend("Reference theta(t)", "theta(t)");         % Add legend for clarity
+% grid on;
+% grid minor;
 % 
 % % Small box with zoomed part
 % axes('position', [.65 .25 .25 .25]); % Position of the zoom box
@@ -87,3 +89,12 @@ grid minor;
 % grid on; % Ensure grid is on in zoomed area
 % grid minor; % Minor grid lines in zoomed area
 % hold off;
+% omega = reshape(permute(out.ScopeData1.signals.values, [3, 1, 2]), 5001, 1);
+% figure;
+% hold on;
+% plot(time1(index), omega(index), 'r-');
+% xlabel('Time [s]');
+% ylabel('Omega [rad/s]');
+% legend("Omega(t)");         % Add legend for clarity
+% grid on;
+% grid minor;
