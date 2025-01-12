2d9c86a8343729d1f865d2c3cd5eec03eb7dd616
thomas397 <64840739+thomas397@users.noreply.github.com>
2025-01-12 14:54:55 +0100

    updates

--- Trajectory/gains.m
+++ Trajectory/gains.m
@@ -1,4 +1,4 @@
-controller = 1; %1 = feedback_zd, 2 = feedback_yd
+controller = 2; %1 = feedback_zd, 2 = feedback_yd
 
 switch controller
     case 1
@@ -7,9 +7,7 @@ switch controller
         k_d1 = 90;
         k_d2 = 90;
 
-        K=[ k_p1, 0, k_d1, 0; 
-            0, k_p2, 0, k_d2  ];
-
+    
     case 2
         k_p1 = 45;
         k_p2 = 30;
